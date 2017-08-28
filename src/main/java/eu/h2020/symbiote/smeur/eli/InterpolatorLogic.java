c406f4c362aa3a1a0b3265f24e7e0ece4985f8ba
mkusek <mario.kusek@fer.hr>
2017-08-28 03:30:43 +0200

    cleanup

--- src/main/java/eu/h2020/symbiote/smeur/eli/InterpolatorLogic.java
+++ src/main/java/eu/h2020/symbiote/smeur/eli/InterpolatorLogic.java
@@ -20,30 +20,30 @@ import eu.h2020.symbiote.smeur.eli.model.MessageResponse;
 
 @Component
 public class InterpolatorLogic implements ProcessingLogic {
-	private static final Logger log = LoggerFactory.getLogger(InterpolatorLogic.class);
-	
-	private EnablerLogic enablerLogic;
-
-	@Override
-	public void initialization(EnablerLogic enablerLogic) {
-		this.enablerLogic = enablerLogic;
+    private static final Logger log = LoggerFactory.getLogger(InterpolatorLogic.class);
     
-		asyncCommunication();
-		syncCommunication();
+    private EnablerLogic enablerLogic;
 
-		queryFixedStations();
-		queryMobileStations();
-		
-	}
+    @Override
+    public void initialization(EnablerLogic enablerLogic) {
+        this.enablerLogic = enablerLogic;
+    
+        asyncCommunication();
+        syncCommunication();
+
+        queryFixedStations();
+        queryMobileStations();
+        
+    }
 
     private void asyncCommunication() {
         // asynchronous communications to another Enabler Logic component
-		// register consumer for message type EnablerLogicDataAppearedMessage
+        // register consumer for message type EnablerLogicDataAppearedMessage
         enablerLogic.registerAsyncMessageFromEnablerLogicConsumer(
             EnablerLogicDataAppearedMessage.class, 
             (m) -> log.info("Received from another EnablerLogic: {}", m));
-		
-		// send myself async message
+        
+        // send myself async message
         enablerLogic.sendAsyncMessageToEnablerLogic("EnablerLogicInterpolator", new EnablerLogicDataAppearedMessage());
     }
     
@@ -63,56 +63,56 @@ public class InterpolatorLogic implements ProcessingLogic {
         log.info("Received sync response: {}", response.getResponse());
     }
 
-	@Override
-	public void measurementReceived(EnablerLogicDataAppearedMessage dataAppeared) {
-		System.out.println("received new Observations:\n"+dataAppeared);
-	}
-	
-	private void queryFixedStations() {
-		ResourceManagerTaskInfoRequest request = new ResourceManagerTaskInfoRequest();
-		request.setTaskId("Vienna-Fixed");
-		request.setEnablerLogicName("interpolator");
-		request.setMinNoResources(1);
-		request.setCachingInterval("P0000-00-00T00:10:00"); // 10 mins.
-		    // Although the sampling period is either 30 mins or 60 mins there is a transmit
-			// delay.
-			// If we miss one reading by just 1 second and we set the interval to 30 mins we
-			// are always 29 mins and 59 late.
-		CoreQueryRequest coreQueryRequest = new CoreQueryRequest();
-		coreQueryRequest.setLocation_lat(48.208174);
-		coreQueryRequest.setLocation_long(16.373819);
-		coreQueryRequest.setMax_distance(10_000); // radius 10km
-		coreQueryRequest.setObserved_property(Arrays.asList("NOx"));
-		request.setCoreQueryRequest(coreQueryRequest);
-		ResourceManagerAcquisitionStartResponse response = enablerLogic.queryResourceManager(request);
+    @Override
+    public void measurementReceived(EnablerLogicDataAppearedMessage dataAppeared) {
+        System.out.println("received new Observations:\n"+dataAppeared);
+    }
+    
+    private void queryFixedStations() {
+        ResourceManagerTaskInfoRequest request = new ResourceManagerTaskInfoRequest();
+        request.setTaskId("Vienna-Fixed");
+        request.setEnablerLogicName("interpolator");
+        request.setMinNoResources(1);
+        request.setCachingInterval("P0000-00-00T00:10:00"); // 10 mins.
+            // Although the sampling period is either 30 mins or 60 mins there is a transmit
+            // delay.
+            // If we miss one reading by just 1 second and we set the interval to 30 mins we
+            // are always 29 mins and 59 late.
+        CoreQueryRequest coreQueryRequest = new CoreQueryRequest();
+        coreQueryRequest.setLocation_lat(48.208174);
+        coreQueryRequest.setLocation_long(16.373819);
+        coreQueryRequest.setMax_distance(10_000); // radius 10km
+        coreQueryRequest.setObserved_property(Arrays.asList("NOx"));
+        request.setCoreQueryRequest(coreQueryRequest);
+        ResourceManagerAcquisitionStartResponse response = enablerLogic.queryResourceManager(request);
 
-		try {
-			log.info("querying fixed resources: {}", new ObjectMapper().writeValueAsString(response));
-		} catch (JsonProcessingException e) {
-			log.error("Problem with deserializing ResourceManagerAcquisitionStartResponse", e);
-		}
-	}
+        try {
+            log.info("querying fixed resources: {}", new ObjectMapper().writeValueAsString(response));
+        } catch (JsonProcessingException e) {
+            log.error("Problem with deserializing ResourceManagerAcquisitionStartResponse", e);
+        }
+    }
 
-	private void queryMobileStations() {
-		
-		ResourceManagerTaskInfoRequest request = new ResourceManagerTaskInfoRequest();
-		request.setTaskId("Vienna-Mobile");
-		request.setEnablerLogicName("interpolator");
-		request.setMinNoResources(1);
-		request.setCachingInterval("P0000-00-00T00:01:00"); // 1 min
+    private void queryMobileStations() {
+        
+        ResourceManagerTaskInfoRequest request = new ResourceManagerTaskInfoRequest();
+        request.setTaskId("Vienna-Mobile");
+        request.setEnablerLogicName("interpolator");
+        request.setMinNoResources(1);
+        request.setCachingInterval("P0000-00-00T00:01:00"); // 1 min
 
-		CoreQueryRequest coreQueryRequest = new CoreQueryRequest();
-		coreQueryRequest.setLocation_lat(48.208174);
-		coreQueryRequest.setLocation_long(16.373819);
-		coreQueryRequest.setMax_distance(10_000); // radius 10km
-		coreQueryRequest.setObserved_property(Arrays.asList("NOx"));
-		request.setCoreQueryRequest(coreQueryRequest);
-		ResourceManagerAcquisitionStartResponse response = enablerLogic.queryResourceManager(request);
+        CoreQueryRequest coreQueryRequest = new CoreQueryRequest();
+        coreQueryRequest.setLocation_lat(48.208174);
+        coreQueryRequest.setLocation_long(16.373819);
+        coreQueryRequest.setMax_distance(10_000); // radius 10km
+        coreQueryRequest.setObserved_property(Arrays.asList("NOx"));
+        request.setCoreQueryRequest(coreQueryRequest);
+        ResourceManagerAcquisitionStartResponse response = enablerLogic.queryResourceManager(request);
 
-		try {
-			log.info("querying mobile resources: {}", new ObjectMapper().writeValueAsString(response));
-		} catch (JsonProcessingException e) {
-			log.error("Problem with deserializing ResourceManagerAcquisitionStartResponse", e);
-		}
-	}
+        try {
+            log.info("querying mobile resources: {}", new ObjectMapper().writeValueAsString(response));
+        } catch (JsonProcessingException e) {
+            log.error("Problem with deserializing ResourceManagerAcquisitionStartResponse", e);
+        }
+    }
 }
