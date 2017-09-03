da602f624bea5c0a4adb845797b451fb360d43b1
mkusek <mario.kusek@fer.hr>
2017-09-03 23:55:18 +0200

    Added example for registration resources

--- src/main/java/eu/h2020/symbiote/smeur/eli/EnablerLogicInterpolator.java
+++ src/main/java/eu/h2020/symbiote/smeur/eli/EnablerLogicInterpolator.java
@@ -2,15 +2,19 @@ package eu.h2020.symbiote.smeur.eli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * First version of the enabler logic (interpolation)
  * @author DuennebeilG
+ * @author Mario Kušek <mario.kusek@fer.hr>
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EnablerLogicInterpolator {
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
 		SpringApplication.run(EnablerLogicInterpolator.class, args);
 	}
 }
--- src/main/java/eu/h2020/symbiote/smeur/eli/InterpolatorLogic.java
+++ src/main/java/eu/h2020/symbiote/smeur/eli/InterpolatorLogic.java
@@ -4,14 +4,22 @@ import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import eu.h2020.symbiote.EnablerLogic;
-import eu.h2020.symbiote.ProcessingLogic;
+import eu.h2020.symbiote.enablerlogic.EnablerLogic;
+import eu.h2020.symbiote.enablerlogic.ProcessingLogic;
+import eu.h2020.symbiote.enablerlogic.messaging.RegistrationHandlerClientService;
+import eu.h2020.symbiote.enablerlogic.messaging.properties.EnablerLogicProperties;
+import eu.h2020.symbiote.cloud.model.CloudResourceParams;
+import eu.h2020.symbiote.cloud.model.internal.CloudResource;
 import eu.h2020.symbiote.core.internal.CoreQueryRequest;
+import eu.h2020.symbiote.core.model.WGS84Location;
+import eu.h2020.symbiote.core.model.resources.FeatureOfInterest;
+import eu.h2020.symbiote.core.model.resources.StationarySensor;
 import eu.h2020.symbiote.enabler.messaging.model.EnablerLogicDataAppearedMessage;
 import eu.h2020.symbiote.enabler.messaging.model.ResourceManagerAcquisitionStartResponse;
 import eu.h2020.symbiote.enabler.messaging.model.ResourceManagerTaskInfoRequest;
@@ -23,17 +31,52 @@ public class InterpolatorLogic implements ProcessingLogic {
     private static final Logger log = LoggerFactory.getLogger(InterpolatorLogic.class);
     
     private EnablerLogic enablerLogic;
+    
+    @Autowired
+    private EnablerLogicProperties props;
+    
+    @Autowired
+    private RegistrationHandlerClientService rhClientService;
 
     @Override
     public void initialization(EnablerLogic enablerLogic) {
         this.enablerLogic = enablerLogic;
-    
-        asyncCommunication();
-        syncCommunication();
 
-        queryFixedStations();
-        queryMobileStations();
+        registerResources();
         
+        //asyncCommunication();
+        //syncCommunication();
+
+        //queryFixedStations();
+        //queryMobileStations();
+    }
+
+    private void registerResources() {
+        CloudResource cloudResource = new CloudResource();
+        cloudResource.setInternalId("1600");
+        cloudResource.setPluginId(props.getEnablerName());
+        cloudResource.setCloudMonitoringHost("cloudMonitoringHostIP");
+
+        StationarySensor sensor = new StationarySensor();
+        cloudResource.setResource(sensor);
+        sensor.setLabels(Arrays.asList("lamp"));
+        sensor.setComments(Arrays.asList("A comment"));
+        sensor.setInterworkingServiceURL("https://symbiote-h2020.eu/example/interworkingService/");
+        sensor.setLocatedAt(new WGS84Location(2.349014, 48.864716, 15, 
+                Arrays.asList("Paris"), 
+                Arrays.asList("This is Paris")));
+        FeatureOfInterest featureOfInterest = new FeatureOfInterest();
+        sensor.setFeatureOfInterest(featureOfInterest);
+        featureOfInterest.setLabels(Arrays.asList("Room1"));
+        featureOfInterest.setComments(Arrays.asList("This is room 1"));
+        featureOfInterest.setHasProperty(Arrays.asList("temperature"));
+        sensor.setObservesProperty(Arrays.asList("temperature,humidity".split(",")));
+        
+        CloudResourceParams cloudResourceParams = new CloudResourceParams();
+        cloudResource.setParams(cloudResourceParams);
+        cloudResourceParams.setType("Type of device, used in monitoring");
+
+        rhClientService.registerResource(cloudResource);
     }
 
     private void asyncCommunication() {
